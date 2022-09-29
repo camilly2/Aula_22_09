01a1e2e132b2486f0203f28d6b022d32dd3c504e
camilly2 <camillysugai.9445@aluno.saojudas.br>
2022-09-29 11:48:58 -0300

    termino_257
    Final da aula 29/09, deu erro no código, não é reconhecido o pessoa.

---
+++ Aula_29_09/terceiroprojeto/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'terceiroprojeto';
+  textoBotao = "Esconder";
+  esconder = false;
+
+  pessoas = [
+    { nome: "José", idade: 18 },
+    { nome: "Maria", idade: 22 },
+    { nome: "Camilly", idade: 19}
+  ];
+
+  alterarExibicao() {
+    this.textoBotao = this.esconder ? "Exibir" : "Esconder";
+    this.esconder = !this.esconder;
+  }
+
+  adicionar(nome: any, idade: any) {
+    this.pessoas = [{ nome: nome, idade: idade }, ...this.pessoas];
+    }
+    
+}
