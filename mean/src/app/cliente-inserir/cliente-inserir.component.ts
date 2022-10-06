bfad3cc5328efe0f4a9f44e77b269c25becaada1
camilly2 <camillysugai.9445@aluno.saojudas.br>
2022-10-06 11:53:59 -0300

    maen

---
+++ mean/src/app/cliente-inserir/cliente-inserir.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-inserir',
+  templateUrl: './cliente-inserir.component.html',
+  styleUrls: ['./cliente-inserir.component.css']
+})
+export class ClienteInserirComponent implements OnInit {
+
+  nome: string | undefined;
+  fone: string | undefined;
+  email: string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAdicionarClientes(){
+    console.log('Adicionando Cliente');
+  }
+
+}
